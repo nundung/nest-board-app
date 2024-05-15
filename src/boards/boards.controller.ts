f350046898dbe427b13474b106651c50906af756
nundung <jjy809123@naver.com>
2024-05-15 20:11:35 +0900

    Feat: BoardsController 클래스에 createBoard 매서드 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
@@ -8,5 +9,13 @@ export class BoardsController {
     @Get('/')
     getAllBoard() {
         return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Board {
+        return this.boardsService.createBoard(title, description);
     }
 }
