cd13d82dff676b5f6e65761bdf1bbbed09ddddeb
nundung <jjy809123@naver.com>
2024-05-16 17:58:36 +0900

    Feat: board 관련 service 매서드들 생성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
 @Injectable()
 export class BoardsService {
     //private을 사용하지 않으면
@@ -19,6 +21,25 @@ export class BoardsService {
             status: BoardStatus.PUBLIC,
         };
         this.boards.push(board);
+        return board;
+    }
+
+    getBoardById(id: string): Board {
+        const found = this.boards.find((board) => board.id === id);
+        if (!found) {
+            throw new NotFoundException(`Can't find Board with id ${id}`);
+        }
+        return found;
+    }
+
+    deleteBoard(id: string): void {
+        const found = this.getBoardById(id);
+        this.boards = this.boards.filter((board) => board.id !== found.id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
         return board;
     }
 }
