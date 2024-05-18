e3872030f0420e8918c90ed1ef07f359b5476781
nundung <jjy809123@naver.com>
2024-05-19 00:55:05 +0900

    Fix: typeOrmConfig 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,15 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: process.env.PSQL_HOST,
-    port: parseInt(process.env.PSQL_PORT),
-    username: process.env.PSQL_USER,
-    password: process.env.PSQL_PW,
-    database: process.env.PSQL_DATABASE,
+    host: 'localhost',
+    port: 5433,
+    username: 'postgres',
+    password: '1234',
+    database: 'board-app',
     entities: [__dirname + '../**/*.entity.{js,ts}'],
     synchronize: true,
 };
