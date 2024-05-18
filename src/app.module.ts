f27816a872f4cf4bd8343c3d64a131104e8a09c2
nundung <jjy809123@naver.com>
2024-05-19 02:01:31 +0900

    Feat: appModule 커스텀레포지토리 import 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
+import { BoardsController } from './boards/boards.controller';
+import { BoardsService } from './boards/boards.service';
+import { TypeOrmExModule } from './configs/typeorm-ex.module';
+import { BoardRepository } from './boards/board.repository';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+    imports: [
+        TypeOrmModule.forRoot(typeORMConfig),
+        BoardsModule,
+        TypeOrmExModule.forCustomRepository([BoardRepository]),
+    ],
+    controllers: [BoardsController],
+    providers: [BoardsService],
 })
 export class AppModule {}
