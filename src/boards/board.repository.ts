efced13e11e402c7508c511f0acb191de04bf5c4
nundung <jjy809123@naver.com>
2024-05-19 16:02:18 +0900

    Feat: boardRepository에 createBoardDto 생성

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,6 +1,20 @@
 import { Repository } from 'typeorm';
 import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
 import { Board } from './board.entity';
+import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatus } from './board-status.enum';
 
 @CustomRepository(Board)
-export class BoardRepository extends Repository<Board> {}
+export class BoardRepository extends Repository<Board> {
+    async CreateBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+        const { title, description } = createBoardDto;
+
+        const board = this.create({
+            title: title,
+            description: description,
+            status: BoardStatus.PUBLIC,
+        });
+        await this.save(board);
+        return board;
+    }
+}
