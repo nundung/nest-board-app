e528d35e4351933d0b05ef76ad322d6168e992f9
nundung <jjy809123@naver.com>
2024-05-19 17:21:26 +0900

    Fix: user.repository 파일 import문 수정

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,6 @@
 import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
-import { User } from './uer.entity';
+import { User } from './user.entity';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {}
