176b0b4d1673e5a16a89d7ea7583d7e27e827b39
nundung <jjy809123@naver.com>
2024-05-19 21:30:18 +0900

    Feat: userRepository에 creatUser 함수 생성

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,25 @@
 import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredetialsDto } from './dto/auth-credential.dto';
+import { ConflictException } from '@nestjs/common';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+    async createUser(authCredentialsDto: AuthCredetialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+
+        const user = this.create({ username: username, password: password });
+
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Existing username');
+            }
+            console.log('error', error);
+        }
+        await this.save(user);
+        return;
+    }
+}
