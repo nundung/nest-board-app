f4d1832bb0771911c99b9ea60999947e3cd6ef05
nundung <jjy809123@naver.com>
2024-05-19 16:53:51 +0900

    Feat: Auth module, controller, service 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { BoardsController } from './boards/boards.controller';
 import { BoardsService } from './boards/boards.service';
 import { TypeOrmExModule } from './configs/typeorm-ex.module';
 import { BoardRepository } from './boards/board.repository';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeORMConfig),
         BoardsModule,
         TypeOrmExModule.forCustomRepository([BoardRepository]),
+        AuthModule,
     ],
     controllers: [BoardsController],
     providers: [BoardsService],
