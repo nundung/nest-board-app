363845e87834f9dde848fd9903c60561f247181f
nundung <jjy809123@naver.com>
2024-05-20 14:18:29 +0900

    Feat: app.module, auth.module에 JwtModule,PassportModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [JwtModule, TypeOrmModule.forFeature([UserRepository])],
     controllers: [AuthController],
     providers: [AuthService],
 })
