c7d07cb53e1f6356f4c23c35071a9a112156c737
nundung <jjy809123@naver.com>
2024-05-20 13:04:09 +0900

    Feat: createUser함수 bcrypt로직 추가

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,23 +1,32 @@
-import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
-import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
-import { ConflictException } from '@nestjs/common';
+import { CustomRepository } from 'src/configs/typeorm-ex.decorator';
+
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import {
+    ConflictException,
+    InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
-        const user = this.create({ username: username, password: password });
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({ username, password: hashedPassword });
+        // const user = this.create({ username: username, password: password });
 
         try {
             await this.save(user);
         } catch (error) {
             if (error.code === '23505') {
                 throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
             }
-            console.log('error', error);
         }
         await this.save(user);
         return;
